be828226923a87c9ee8b661afbf0f1ae84b01c93
samd-990 <samyakd990@gmail.com>
2023-06-29 16:42:49 +0530

    refetch market info on reload

--- app1/views.py
+++ app1/views.py
@@ -1,10 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse,Http404,HttpResponseRedirect
-import time
 from yahooquery import Ticker
 from concurrent.futures import ThreadPoolExecutor
 
-# Create your views here.
 def home(request):
     return render(request,"app1/home.html")
 
@@ -15,9 +12,8 @@ def portfolio(request):
     return render(request,"app1/portfolio.html")
 
 def view_market(request):
-    # price_list.clear()
-    if len(price_list)==0:
-        stock_data()
+    price_list.clear()
+    stock_data()
     price_list.sort(key= lambda x:x['cap'],reverse=True)
     return render(request,"app1/view_market.html",{
         "stock_info":price_list
@@ -44,16 +40,8 @@ def retrieve_data(ticker):
         "change":round(change,2),
         "changepercent":round(changepercent,2)
     })
-    # print(f"{ticker} {Ticker(ticker).price[ticker].get('regularMarketPrice')}")
 
-# def price(s):
-#     for i in range()
-
-# print("start")
-# t0=time.time()
 def stock_data():
     ticker_list=open('app1\stocks.txt','r').read().split('\n')
     with ThreadPoolExecutor(100) as executor:
         executor.map(retrieve_data, ticker_list)
-# print(stock_data())
-# print("end",time.time()-t0)
